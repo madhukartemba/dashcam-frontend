f228c28527ff345eb8db15cb6bd677ddd777bd44
Madhukar Temba <madhusmiles.madhukar@gmail.com>
2024-03-09 19:57:08 +0530

    added traffic light

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateUrl } from './components/redux/action';
-import { useGetStatus } from './hooks/useGetStatus';
 import { useGetData } from './hooks/useGetData';
+import TrafficLight from './components/TrafficLight';
 
 function App(): React.JSX.Element {
 
@@ -13,34 +13,20 @@ function App(): React.JSX.Element {
     dispatch(updateUrl(url))
   }
 
-  const [data, isLoading, error] = useGetData();
-
-
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Test application</Text>
-      <Text style={styles.text}>{error ? "Error" : data?.trafficLightColor} </Text>
-      <Button title="Test Update" onPress={() => handleUpdateUrl("http://192.168.1.19:5000")} />
+      <TrafficLight data={{
+        trafficLightColor: 'red',
+        fps: 10,
+      }} />
     </View>
   );
-}
-
-function UrlDisplay(): React.JSX.Element {
-  const url = useSelector((state: any) => {
-    return state.urlReducer
-  });
-
-  return (
-    <Text style={styles.text}> {url} </Text>
-  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   text: {
     color: 'white',
---
+++ components/TrafficLight.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Data } from '../hooks/useGetData';
+
+type Props = {
+    data: Data;
+};
+
+const TrafficLight: React.FC<Props> = ({ data }) => {
+    const getColorStyle = (color: 'red' | 'green' | 'yellow' | null, bulbColor: 'red' | 'green' | 'yellow') => {
+        if (bulbColor != color) {
+            return styles.gray
+        }
+        switch (color) {
+            case 'red':
+                return styles.red;
+            case 'green':
+                return styles.green;
+            case 'yellow':
+                return styles.yellow;
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.trafficLight}>
+                <View style={[styles.light, getColorStyle(data.trafficLightColor, "red")]} />
+                <View style={[styles.light, getColorStyle(data.trafficLightColor, "yellow")]} />
+                <View style={[styles.light, getColorStyle(data.trafficLightColor, "green")]} />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    trafficLight: {
+        width: 50,
+        height: 150,
+        backgroundColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 5,
+        borderColor: 'white',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    light: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+    },
+    red: {
+        backgroundColor: 'red',
+    },
+    green: {
+        backgroundColor: 'green',
+    },
+    yellow: {
+        backgroundColor: 'yellow',
+    },
+    gray: {
+        backgroundColor: '#333'
+    }
+});
+
+export default TrafficLight;

bfff870055cdbb75faeedd4c42f6442874b8ae42
Madhukar Temba <madhusmiles.madhukar@gmail.com>
2024-03-11 20:42:49 +0530

    always have 2 decimal places

--- components/Inference/Inference.tsx
+++ components/Inference/Inference.tsx
@@ -33,7 +33,7 @@ export const Inference = () => {
     if (data) {
         switch (data.status) {
             case 'recovery':
-                return <Loading text={`Recovering ${parseFloat(data.recoveryPercent.toFixed(2))}%`} />;
+                return <Loading text={`Recovering ${(data.recoveryPercent).toFixed(2)}%`} />;
             case 'inference':
                 actionsInstance.act(data.trafficLightColor);
                 actionsInstance.updateBufferSize(ACTIONS_BUFFER_SIZE_MULTIPLIER * data.fps);

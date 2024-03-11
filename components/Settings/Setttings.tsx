8b1579e9a0dfec7210850a1db71b58cd92f6cced
Madhukar Temba <madhusmiles.madhukar@gmail.com>
2024-03-11 11:44:45 +0530

    updated settings page

--- components/Settings/Setttings.tsx
+++ components/Settings/Setttings.tsx
@@ -1,17 +1,54 @@
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { RootStackParamList } from '../../App';
+import React, { useState } from 'react';
+import { View, Text, Button, TextInput, StyleSheet, Alert } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUrl } from '../redux/action';
 
-type Props = { navigation: NativeStackNavigationProp<RootStackParamList, 'Settings'> }
+function Settings() {
 
-function Settings({ navigation }: Props) {
+    const currentUrl = useSelector((state: any) => state.urlReducer);
+
+    const [url, setUrl] = useState(currentUrl);
+
+    const dispatcher = useDispatch();
+
+    const handleUrlUpdate = () => {
+        dispatcher(updateUrl(url));
+        Alert.alert('URL Updated', 'The URL has been updated successfully.');
+    }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Settings Screen</Text>
-            <Button title="Go Back" onPress={() => navigation.goBack()} />
+        <View style={styles.container}>
+            <Text style={styles.text}>Server URL</Text>
+            <TextInput
+                value={url}
+                onChangeText={(text) => setUrl(text)}
+                style={styles.input}
+            />
+            <Button title="Update URL" onPress={handleUrlUpdate} />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'black',
+    },
+    text: {
+        color: 'white',
+        marginBottom: 10,
+    },
+    input: {
+        color: 'white',
+        borderWidth: 3,
+        borderColor: 'gray',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+        width: '50%',
+    },
+});
 
 export default Settings;
